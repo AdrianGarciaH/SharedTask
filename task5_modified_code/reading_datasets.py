a4ffe34ea1d3458993523c872c42916394f6990f
Ubuntu <ubuntu@ip-10-0-14-121.us-east-2.compute.internal>
2022-07-19 14:26:27 +0000

    added clean code

---
+++ task5_modified_code/reading_datasets.py
@@ -0,0 +1,58 @@
+import pickle
+import json
+import csv
+
+def load_data(filename):
+    a_file = open(filename, "rb")
+    output = pickle.load(a_file)
+    a_file.close()
+    return output
+
+def read_task5(location, split = 'train'):
+    filename = location + split + '.tsv'
+
+    data = []
+    with open(filename) as tsv_file:
+        tsv_reader = csv.reader(tsv_file, delimiter='\t')
+        for i, row in enumerate(tsv_reader):
+            if i > 0:
+                tweet_id = row[0]
+                sentence = row[1].strip()
+                label = row[2]
+                data.append((tweet_id, sentence, label))
+
+    return data
+
+def read_test(location, split = 'test'):
+    filename = location + split + '.tsv'
+
+    data = []
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='\t')
+        for i, row in enumerate(csv_reader):
+            if i > 0:
+                tweet_id = row[0]
+                sentence = row[1].strip()
+                data.append((tweet_id, sentence))
+
+    return data
+
+
+if __name__ == '__main__':
+    location = '../2022.07.07_task5/'
+    split = 'train'
+    
+    data = read_task5(location, split)
+    print(len(data))
+
+    data = read_task5(location, 'dev')
+    print(len(data))
+    
+    data = read_task5(location, 'test')
+    print(len(data))
+
+    
+
+
+
+
